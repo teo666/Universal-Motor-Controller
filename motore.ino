db6c481509b6eeefe24c1850634c7e7a760ca8b9
Teo Basili <basili.teo@gmail.com>
2018-08-03 20:47:59 +0000

    pulizia codice

--- motore.ino
+++ motore.ino
@@ -1,59 +1,6 @@
 #include <PID_ASYNC.h>
 #include <EEPROM.h>
-
-
-//registro per l'IO B (PORTD)
-//pin per controllare il triac (13 su Arduino UNO)
-#define TRIAC_OUTPUT 7
-
-//pin per la programmazione portD
-#define PROG_PIN 6
-
-//pin per l'input dello zcd (PORTD) non puo' cambiare a causa dell'interruzione abilitata
-#define ZCD_INPUT 2
-//pin per l'input del tacho (PORTD) non puo' cambiare a causa dell'interruzione abilitata
-#define TACHO_INPUT 3
-
-// pin per la lettura del valore del potenziometro per regolare la velocita' (PORTA)
-#define SPEED_READ 0
-
-//ZERO CROSSING DETECTOR LOG per il debug su oscilloscopio
-#define ZCD_LOG 4
-
-//TRIAC LOG per il debug del triac, stesso comportamento di TRIAC_OUTPUT
-#define TRIAC_LOG 5
-
-//TACHO LOG per il debug del tacho
-#define TACHO_LOG 3
-
-//identifica il minimo intervallo di tempo fra le creste di salita del
-//segnale di tacho
-//#define READ_UPPER_SPEED_LIMIT 70
-//identifica il massimo intervallo di tempo fra le creste di salita del
-//segnale di tacho,
-//#define READ_LOWER_SPEED_LIMIT 260
-
-//minima velocita' desiderata, valori alti, abbassano la velocita
-//se 0 il motore riceve sempre il massimo della potenza
-//#define MIN_SPEED_THRESHOLD 470
-
-//definizione delle macro di accensione e espegnimento dei diversi pin
-#define TURN_ON_TRIAC() sbi(PORTD,TRIAC_OUTPUT)
-#define TURN_OFF_TRIAC() cbi(PORTD,TRIAC_OUTPUT)
-#define TURN_ON_ZDC_LOG() sbi(PORTB,ZCD_LOG)
-#define TURN_OFF_ZDC_LOG() cbi(PORTB,ZCD_LOG)
-#define TURN_ON_TRIAC_LOG() sbi(PORTB,TRIAC_LOG)
-#define TURN_OFF_TRIAC_LOG() cbi(PORTB,TRIAC_LOG)
-#define TURN_ON_TACHO_LOG() sbi(PORTB,TACHO_LOG)
-#define TURN_OFF_TACHO_LOG() cbi(PORTB,TACHO_LOG)
-#define ANALOG_READ() sbi(ADCSRA,6)
-
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
+#include "pin_definition.h"
 
 volatile uint16_t zcd_tick_log = 0;
 volatile uint16_t tacho_tick_log = 0;
@@ -108,8 +55,6 @@ uint16_t output_max_speed_value = 0;
 //variabili del pid
 
 double Setpoint, Input, Output;
-//double Kp=1.2, Ki=20, Kd=0.1;
-//double Kp=2, Ki=00.0001, Kd=0;
 volatile uint8_t computeBarrier = 0;
 
 CoefficientPtr aaa = 0;
@@ -129,7 +74,7 @@ CoefficientPtr search(){
   return &bbb;
 }
 
-/////
+
 String serial_read;
 
 PID *motor_PID;
@@ -223,31 +168,15 @@ void save_high_speed_exit_fun(){
 }
 
 void setup() {
-  //setta i pin definiti sulla porta b come uscite
-  sbi(DDRB,ZCD_LOG);
-  sbi(DDRB,TRIAC_LOG);
-  sbi(DDRB,TACHO_LOG);
-  
-  //setta i pin definiti sulla porta d come uscite
-  sbi(DDRD,TRIAC_OUTPUT);
 
-  //setta i pin di ingresso del tacho e dello zcd come ingressi
-  //TODO : abilitare il pulldown???
-  cbi(DDRD,TACHO_INPUT);
-  cbi(DDRD,ZCD_INPUT);
-  sbi(PORTD,TACHO_INPUT);
-  //setta il pin di programmazione come ingresso e abilita la resistenza di pullup
-  //permette di attaccare un push button senza ulteriori componenti
-  //(ad essere pignoli servirebbe un condensatore in parallelo,
-  //ma non lavoriamo in interrupt mode, quindi va bene ugualmente)
-  cbi(DDRD,PROG_PIN);
-  sbi(PORTD,PROG_PIN);
+  INIT_TRIAC_LOG();
+  INIT_ZCD_LOG();
+  INIT_TACHO_LOG();
 
-  //setta tutte le uscite LOW
-  cbi(PORTD,TRIAC_OUTPUT);
-  cbi(PORTB,ZCD_LOG);
-  cbi(PORTB,TRIAC_LOG);
-  cbi(PORTB,TACHO_LOG);
+  INIT_TRIAC();
+  INIT_PROG_BUTTON();
+  INIT_ZCD();
+  INIT_TACHO();
 
   //abilita le interruzioni sul pin 3 e 2 di arduino (bit meno significativo e' il 2) pin 3 = int1 pin 2 = int0
   EIMSK = 0b00000011;
@@ -265,26 +194,17 @@ void setup() {
   TCCR2B = 0b00000001;
   TIMSK2 = 0b00000001;
 
-  //configurazione timer 1 16 bit
-  //TCCR1A = 0b00000001;
-  //TCCR1B = 0b00001001;
-
-  //TIMSK1 = 0b00100111;
-
-  //configurazione timer 0
-  //TCCR0A = 0b00000011;
-  //TCCR0B = 0b00000001;
-
-   
-  //TIMSK0 = 0b00000000;
+  //disabilito interruzioni timer 0
+  TIMSK0 = 0b00000000;
 
   //settaggio della lettura analogica
   //setto il voltaggio di riferimento del convertitore alla tensione di alimentazione
   //e il multiplexer per la lettura
   ADMUX = (1 << 6) | ( SPEED_READ & 0x07);
+  
   //abilito l'ADC, le interruzioni hardware e il prescaler a 128 per letture accurate su 10bit
   ADCSRA = 0b10001111;
-  //non setto le lettura automatiche utilizzando il registro ADCSRB perhce le lancio a mano
+  //non setto le lettura automatiche utilizzando il registro ADCSRB perche le lancio a mano
 
   Serial.begin(9600);
   
@@ -347,15 +267,15 @@ void setup() {
   Serial.println(output_max_speed_value);
   output = output_min_speed_value;
   Output = output;
-  //motor_PID = new PID(&Input, &Output, &Setpoint, Kp, Ki, Kd, P_ON_E, (void *) &computeBarrier, (void *) &tacho_tick_log, DIRECT);
 
   motor_PID = new PID(&Input, &Output, &Setpoint, P_ON_E, &search, &tacho_tick_log, DIRECT);
+  
   //TODO quando sopra un certo livello azzerare il valore di output per rendere il rallentamento migliore
   motor_PID->SetOutputLimits(0,output_min_speed_value);
+  
   motor_PID->SetMode(AUTOMATIC);
-  //motor_PID->SetSampleTime(10);
-  //Serial.end();
-  delay_count = 1;
+
+  Serial.end();
 }
 
 volatile uint8_t _tacho_trig = 0;
