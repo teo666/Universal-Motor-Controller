803c5efd9f301db4a2e93153f944b9e1ea6564aa
Teo Basili <basili.teo@gmail.com>
2018-08-03 21:14:58 +0000

    inclusione libreria PID asincrona nel progetto stesso

---
+++ PID_ASYNC.cpp
@@ -0,0 +1,151 @@
+/**********************************************************************************************
+ * Arduino PID Async Library - Version 1.0.0
+ * by teo Basili <basili.teo@gmail.com> https://github.com/teo666
+ *
+ * this library is based on Brett Beauregard's PID library
+ * <br3ttb@gmail.com> brettbeauregard.com
+ *
+ * This Library is licensed under a GPLv3 License
+ **********************************************************************************************/
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include "PID_ASYNC.h"
+
+PID::PID(double *Input, double *Output, double *Setpoint, uint8_t POn,
+         CoefficientPtr (*searchfun)(), volatile uint16_t *TimeInterval,
+         uint8_t ControllerDirection) {
+  myOutput = Output;
+  myInput = Input;
+  mySetpoint = Setpoint;
+  inAuto = false;
+
+  timeInterval = TimeInterval;
+  searchfunPtr = searchfun;
+
+  PID::SetOutputLimits(0, 255);
+
+  PID::SetControllerDirection(ControllerDirection);
+}
+
+bool PID::Compute() {
+  if (!inAuto) {
+    return false;
+  }
+
+  // it has to find the correct parameters
+  CoefficientPtr KS = searchfunPtr();
+
+  /*Compute all the working error variables*/
+  double input = *myInput;
+  double error = *mySetpoint - input;
+  double dInput = (input - lastInput);
+  outputSum += (KS->Ki * (*timeInterval) * error);
+
+  /*Add Proportional on Measurement, if P_ON_M is specified*/
+  if (!pOnE)
+    outputSum -= KS->Kp * dInput;
+
+  if (outputSum > outMax)
+    outputSum = outMax;
+  else if (outputSum < outMin)
+    outputSum = outMin;
+
+  /*Add Proportional on Error, if P_ON_E is specified*/
+  double output;
+  if (pOnE)
+    output = KS->Kp * error;
+  else
+    output = 0;
+
+  /*Compute Rest of PID Output*/
+  output += outputSum - (KS->Kd / (*timeInterval) * dInput);
+
+  if (output > outMax)
+    output = outMax;
+  else if (output < outMin)
+    output = outMin;
+  *myOutput = output;
+
+  /*Remember some variables for next time*/
+  lastInput = input;
+
+  return true;
+}
+
+void PID::SetTunings(double Kp, double Ki, double Kd, int POn) {
+
+  SetTunings(Kp, Ki, Kd);
+  pOn = POn;
+  pOnE = POn == P_ON_E;
+}
+
+void PID::SetTunings(double Kp, double Ki, double Kd) {
+  if (Kp < 0 || Ki < 0 || Kd < 0)
+    return;
+
+  if (controllerDirection == REVERSE) {
+    kp = -Kp;
+    ki = -Ki;
+    kd = -Kd;
+  } else {
+    kp = Kp;
+    ki = Ki;
+    kd = Kd;
+  }
+}
+
+void PID::SetOutputLimits(double Min, double Max) {
+  if (Min >= Max)
+    return;
+  outMin = Min;
+  outMax = Max;
+
+  if (inAuto) {
+    if (*myOutput > outMax)
+      *myOutput = outMax;
+    else if (*myOutput < outMin)
+      *myOutput = outMin;
+
+    if (outputSum > outMax)
+      outputSum = outMax;
+    else if (outputSum < outMin)
+      outputSum = outMin;
+  }
+}
+
+void PID::SetMode(int Mode) {
+  bool newAuto = (Mode == AUTOMATIC);
+  if (newAuto && !inAuto) { /*we just went from manual to auto*/
+    PID::Initialize();
+  }
+  inAuto = newAuto;
+}
+
+void PID::Initialize() {
+  outputSum = *myOutput;
+  lastInput = *myInput;
+  if (outputSum > outMax)
+    outputSum = outMax;
+  else if (outputSum < outMin)
+    outputSum = outMin;
+}
+
+void PID::SetControllerDirection(int Direction) {
+  if (inAuto && Direction != controllerDirection) {
+    kp = -kp;
+    ki = -ki;
+    kd = -kd;
+  }
+  controllerDirection = Direction;
+}
+
+double PID::GetKp() { return kp; }
+double PID::GetKi() { return ki; }
+double PID::GetKd() { return kd; }
+int PID::GetMode() { return inAuto ? AUTOMATIC : MANUAL; }
+int PID::GetDirection() { return controllerDirection; }
---
+++ PID_ASYNC.h
@@ -0,0 +1,105 @@
+#ifndef PID_ASYNC
+#define PID_ASYNC
+#define LIBRARY_VERSION 1.0.0
+
+typedef struct _coefficient {
+  double Kp;
+  double Ki;
+  double Kd;
+} Coefficient;
+
+typedef Coefficient *CoefficientPtr;
+
+class PID {
+
+public:
+// Constants used in some of the functions below
+#define AUTOMATIC 1
+#define MANUAL 0
+#define DIRECT 0
+#define REVERSE 1
+#define P_ON_M 0
+#define P_ON_E 1
+
+  // commonly used functions
+  // **************************************************************************
+
+  /**
+   * constructor
+   */
+  PID(double *Input, double *Output, double *Setpoint, uint8_t POn,
+      CoefficientPtr (*searchfun)(),
+      volatile uint16_t *TimeInterval, uint8_t ControllerDirection);
+
+  /**
+   * sets PID to either Manual (0) or Auto (non-0)
+   */
+  void SetMode(int Mode);
+
+  /**
+   *
+   * performs the PID calculation.  it should be
+   * called every time loop() cycles. ON/OFF and
+   * calculation frequency can be set using SetMode
+   * SetSampleTime respectively
+   */
+  bool Compute();
+  /**
+   * clamps the output to a specific range. 0-255 by default, but
+   * it's likely the user will want to change this depending on
+   * the application
+   */
+  void SetOutputLimits(double, double);
+
+  // available but not commonly used functions
+  // ********************************************************
+  void SetTunings(
+      double, double, // * While most users will set the tunings once in the
+      double);        //   constructor, this function gives the user the option
+               //   of changing tunings during runtime for Adaptive control
+  void SetTunings(double, double, // * overload for specifying proportional mode
+                  double, int);
+
+  void SetControllerDirection(
+      int); // * Sets the Direction, or "Action" of the controller. DIRECT
+            //   means the output will increase when error is positive. REVERSE
+            //   means the opposite.  it's very unlikely that this will be
+            //   needed once it is set in the constructor.
+  void
+  SetSampleTime(int); // * sets the frequency, in Milliseconds, with which
+                      //   the PID calculation is performed.  default is 100
+
+  // Display functions
+  // ****************************************************************
+  double GetKp();     // These functions query the pid for interal values.
+  double GetKi();     //  they were created mainly for the pid front-end,
+  double GetKd();     // where it's important to know what is actually
+  int GetMode();      //  inside the PID.
+  int GetDirection(); //
+
+private:
+  void Initialize();
+
+  double kp; // * (P)roportional Tuning Parameter
+  double ki; // * (I)ntegral Tuning Parameter
+  double kd; // * (D)erivative Tuning Parameter
+
+  int controllerDirection;
+  int pOn;
+
+  double *myInput;  // * Pointers to the Input, Output, and Setpoint variables
+  double *myOutput; //   This creates a hard link between the variables and the
+  double
+      *mySetpoint; //   PID, freeing the user from having to constantly tell us
+                   //   what these values are.  with pointers we'll just know.
+
+  // unsigned long lastTime;
+  double outputSum, lastInput;
+  volatile uint16_t *timeInterval;
+
+  CoefficientPtr (*searchfunPtr)();
+  // unsigned long SampleTime;
+  double outMin, outMax;
+  bool inAuto, pOnE;
+};
+#endif
